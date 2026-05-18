17558ce8e24e332fd273501d55c3ee6c4123d7bf
Anders-udvikler <anders99fib@gmail.com>
2026-05-18 19:23:09 +0200

    user added

--- models/User.cs
+++ models/User.cs
@@ -1,10 +1,20 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 namespace Users
 {
     public class User
     {
-        public int Id { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("username")]
         public string Username { get; set; }
+
+        [BsonElement("email")]
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
+
+        [BsonElement("password")]
+        public string Password { get; set; }
     }
 }
